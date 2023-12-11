48ed5ac01c2b9687dea19270e25d76fd02f4ee61
Janvi Pagariya <71075235+Janvi161945@users.noreply.github.com>
2023-12-11 10:55:17 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+
+# Path to the input video file for analysis
+VIDEO_PATH = "path/to/your/video.mp4"
+
+# Path to the YOLOX pre-trained model weights file
+MODEL_PATH_YOLOX = "path/to/your/best.pt"
+
+# Path to save the analyzed output video
+OUTPUT_VIDEO_PATH = "path/to/your/results.mp4"
+
+# Explore additional configurations and adjust according to your needs
+# OTHER_CONFIGURATIONS = ...
+
